e0f043213612328c2d7346cff87d5c7496cdf2ab
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2025-04-19 07:13:04 +0200

    Further simplify test

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,14 @@
 use lender::prelude::{Lender, Lending};
 
 struct Child<'lend> {
-    data: &'lend mut Data,
+    stack: &'lend mut Vec<usize>,
     returned: bool,
 }
 
 impl<'lend> From<&'lend mut InnerParent> for Child<'lend> {
     fn from(parent: &'lend mut InnerParent) -> Self {
         let mut child = Self {
-            data: &mut parent.data,
+            stack: &mut parent.stack,
             returned: false,
         };
         child.push_to_stack(0);
@@ -20,32 +20,32 @@ impl<'lend> Child<'lend> {
     fn push_to_stack(&mut self, row_id: usize) {
         println!(
             "Pushing row {row_id} to stack at address {}",
-            self.data.stack.as_ptr() as usize
+            self.stack.as_ptr() as usize
         );
-        self.data.stack.push(row_id);
+        self.stack.push(row_id);
         println!(
             "Stack at address {} after push: {:?}",
-            self.data.stack.as_ptr() as usize,
-            self.data.stack
+            self.stack.as_ptr() as usize,
+            self.stack
         );
     }
 
     fn pop_from_stack(&mut self) {
         println!(
             "Popping row {} from stack at address {}",
-            self.data.stack.last().unwrap(),
-            self.data.stack.as_ptr() as usize
+            self.stack.last().unwrap(),
+            self.stack.as_ptr() as usize
         );
         println!(
             "Stack at address {} before pop: {:?}",
-            self.data.stack.as_ptr() as usize,
-            self.data.stack
+            self.stack.as_ptr() as usize,
+            self.stack
         );
-        self.data.stack.pop().unwrap();
+        self.stack.pop().unwrap();
         println!(
             "Stack at address {} after pop: {:?}",
-            self.data.stack.as_ptr() as usize,
-            self.data.stack
+            self.stack.as_ptr() as usize,
+            self.stack
         );
     }
 }
@@ -61,18 +61,14 @@ impl<'lend> Lender for Child<'lend> {
             None
         } else {
             self.returned = true;
-            Some(&self.data.stack)
+            Some(&self.stack)
         }
     }
 }
 
-struct Data {
-    stack: Vec<usize>,
-}
-
 struct InnerParent {
     /// The underlying data structure for the algorithm.
-    data: Data,
+    stack: Vec<usize>,
     current_root_id: usize,
 }
 
@@ -84,10 +80,10 @@ impl Lender for InnerParent {
     fn next<'lend>(&'lend mut self) -> Option<<Self as Lending<'lend>>::Lend> {
         while self.current_root_id < 5 {
             debug_assert!(
-                self.data.stack.is_empty(),
+                self.stack.is_empty(),
                 "Stack at address {} should be empty at the start of the inner child loop, but in parent is {:?}",
-                self.data.stack.as_ptr() as usize,
-                self.data.stack
+                self.stack.as_ptr() as usize,
+                self.stack
             );
 
             self.current_root_id += 1;
@@ -113,7 +109,7 @@ impl<'parent> Iterator for Parent<'parent> {
 
 fn main() {
     let inner_parent = InnerParent {
-        data: Data { stack: Vec::new() },
+        stack: Vec::new(),
         current_root_id: 0,
     };
     let parent = Parent {
